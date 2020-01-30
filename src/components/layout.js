dbf7b3e9ab33c8474764a70954ed78a3c02dbd24
Giuseppe Leone <gpleone@gmail.com>
2020-01-30 12:38:24 +0100

    added navbar

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,9 +8,17 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import Navbar from './Globals/Navbar'
+
 
 import "./layout.css"
 
-const Layout = (props) => <>{ props.children }</>;
+const Layout = ({children}) => 
+  <div>
+    <Navbar />
+    {children}
+  </div>
 
 export default Layout
+
+import React, { Component } from 'react'
